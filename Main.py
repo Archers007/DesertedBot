34c32954226ed4435c819a598e1e23ffae65f9e9
Kieran <Kieran@bendell.ca>
2020-11-07 15:21:35 -0500

    Unload and Loadall work proporly

--- Main.py
+++ Main.py
@@ -8,46 +8,126 @@ from discord import Status
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN2')
 
-client = commands.Bot(command_prefix='!')
+CP = ['!',]
+channel_list = []
+emojii = []
+deleteUser = []
+deleteUserChannel = []
+UserSend = []
+Message1 = []
+Message2 = []
+unloaded = []
+extensionsrun = []
 
-extensionsrun = [
 
-    ]
+client = commands.Bot(command_prefix=CP)
+
 class Loaders():
     @client.command()
     async def load(ctx, extension):
         client.load_extension(f'Cog1.{extension}')
         extensionsrun.append(extension)
-        print(extensionsrun)
         await ctx.channel.send("{} has been successfully loaded".format(extension))
     @client.command()
     async def unload(ctx, extension):
         client.unload_extension(f'Cog1.{extension}')
         extensionsrun.remove(extension)
-        print(extensionsrun)
         await ctx.channel.send("{} has been successfully unloaded".format(extension))
     
     @client.command()
     async def unloadall(ctx):
-        await ctx.channel.send('Successfully unloaded all Cogs')
-        for filename in os.listdir('./Cog1'):
-           if filename.endswith('.py'):
-               client.unload_extension(f'Cog1.{filename[:-3]}')
-               extensionsrun.remove(f'{filename[:-3]}')
+        try:
+            client.unload_extension(f'Cog1.{"Dm"}')
+            extensionsrun.remove('Dm')
+        except:
+            print('Dm is already unloaded')
+        try:
+            client.unload_extension(f'Cog1.{"UserInfo"}')
+            extensionsrun.remove('UserInfo')
+        except:
+            print('UserInfo is already unloaded')
+        try:
+            client.unload_extension(f'Cog1.{"ServerInfo"}')
+            extensionsrun.remove('ServerInfo')
+        except:
+            print('ServerInfo is already unloaded')
+        try:
+            client.unload_extension(f'Cog1.{"meme"}')
+            extensionsrun.remove('meme')
+        except:
+            print('meme is already unloaded')
+        try:
+            client.unload_extension(f'Cog1.{"AutoReact"}')
+            extensionsrun.remove('AutoReact')
+        except:
+            print('AutoReact is already unloaded')
+        try:
+            client.unload_extension(f'Cog1.{"CustomCommands"}')
+            extensionsrun.remove('CustomCommands')
+        except:
+            print('CustomCommands is already unloaded')
+        try:
+            client.unload_extension(f'Cog1.{"BanTxT"}')
+            extensionsrun.remove('BanTxT')
+        except:
+            print('BanTxT is already unloaded')
     
     @client.command()
     async def loadall(ctx):
-        await ctx.channel.send('Successfully loaded all Cogs')
-        for filename in os.listdir('./Cog1'):
-            if filename.endswith('.py'):
-                client.load_extension(f'Cog1.{filename[:-3]}')
-                extensionsrun.append(f'{filename[:-3]}')
+        try:
+            client.load_extension(f'Cog1.{"Dm"}')
+            extensionsrun.append('Dm')
+        except:
+            print('Dm is already loaded')
+        try:
+            client.load_extension(f'Cog1.{"UserInfo"}')
+            extensionsrun.append('UserInfo')
+        except:
+            print('UserInfo is already loaded')
+        try:
+            client.load_extension(f'Cog1.{"ServerInfo"}')
+            extensionsrun.append('ServerInfo')
+        except:
+            print('ServerInfo is already loaded')
+        try:
+            client.load_extension(f'Cog1.{"meme"}')
+            extensionsrun.append('meme')
+        except:
+            print('meme is already loaded')
+        try:
+            client.load_extension(f'Cog1.{"AutoReact"}')
+            extensionsrun.append('AutoReact')
+        except:
+            print('AutoReact is already loaded')
+        try:
+            client.load_extension(f'Cog1.{"CustomCommands"}')
+            extensionsrun.append('CustomCommands')
+        except:
+            print('CustomCommands is already loaded')
+        try:
+            client.load_extension(f'Cog1.{"BanTxT"}')
+            extensionsrun.append('BanTxT')
+        except:
+            print('BanTxT is already loaded')
 
 for filename in os.listdir('./Cog1'):
     if filename.endswith('.py'):
         client.load_extension(f'Cog1.{filename[:-3]}')
         extensionsrun.append(f'{filename[:-3]}')
-        print(extensionsrun)
+        
+
+@client.command()
+async def ChangeCP(ctx, args=None):
+    if args !=None:
+        CP.append(args)
+        CP.pop(0)
+        await ctx.channel.send('Successfully Changed Command Prefix to {}'.format(args))
+
+@client.command()
+async def ResetCP(ctx):
+    CP.append('!')
+    CP.pop(0)
+    await ctx.channel.send('Successfully Reset Command Prefix to !')
 
 class RunningInfo():
     @client.command()
@@ -55,33 +135,64 @@ class RunningInfo():
         """Shows Panel Info"""
 
         embeded = discord.Embed(title="Script Panel", description='On/Off', color=0xEE8700)
-        if 'Dm' in extensionsrun:
-            embeded.add_field(name="DM: On", value='Dm' in extensionsrun)
-        else:
-            embeded.add_field(name="DM: On", value='Dm' in extensionsrun)
-            
-        if 'UserInfo' in extensionsrun:
-            embeded.add_field(name="User Info: On", value='UserInfo' in extensionsrun)
-        else:
-            embeded.add_field(name="User Info: On", value='UserInfo' in extensionsrun)
-        if 'ServerInfo' in extensionsrun:
-            embeded.add_field(name="Server Info: On", value='ServerInfo' in extensionsrun)
-        else:
-            embeded.add_field(name="Server Info: On", value='ServerInfo' in extensionsrun)
-        if 'Memes' in extensionsrun:
-            embeded.add_field(name="Meme Generator: On", value='Memes' in extensionsrun)
-        else:
-            embeded.add_field(name="Meme Generator: On", value='Memes' in extensionsrun)
-        if 'TarekRankInsult' in extensionsrun:
-            embeded.add_field(name="Tarek Insult: On", value='TarekRankInsult' in extensionsrun)
-        else:
-            embeded.add_field(name="Tarek Insult: On", value='TarekRankInsult' in extensionsrun)
-
+        embeded.add_field(name="DM: On", value='Dm' in extensionsrun)
+        embeded.add_field(name="User Info: On", value='UserInfo' in extensionsrun)
+        embeded.add_field(name="Server Info: On", value='ServerInfo' in extensionsrun)
+        embeded.add_field(name="Meme Generator: On", value='meme' in extensionsrun)
+        embeded.add_field(name="AutoReact: On", value='AutoReact' in extensionsrun)
+        embeded.add_field(name="Spam: On", value='Spam' in extensionsrun)
+        embeded.add_field(name="BanTxT: On", value='BanTxT' in extensionsrun)
+        embeded.add_field(name="CustomCommands: On", value='CustomCommands' in extensionsrun)
         await ctx.channel.send(embed=embeded)
+    
+    @client.command()
+    async def LOGO(ctx):
+        await ctx.channel.send('╭━━━╮╱╱╱╱╱╱╱╱╱╱╭╮╱╱╱╱╱╭╮╭━━━╮╭━╮╭━╮╱╱╱╱╱╱╱╭╮')
+        await ctx.channel.send('╰╮╭╮┃╱╱╱╱╱╱╱╱╱╭╯╰╮╱╱╱╱┃┃┃╭━╮┃┃╭╯┃╭╯╱╱╱╱╱╱╱┃┃')
+        await ctx.channel.send('╱┃┃┃┣━━┳━━┳━━┳┻╮╭╋━━┳━╯┃┃┃╱┃┣╯╰┳╯╰┳┳━━┳┳━━┫┃')
+        await ctx.channel.send('╱┃┃┃┃┃━┫━━┫┃━┫╭┫┃┃┃━┫╭╮┃┃┃╱┃┣╮╭┻╮╭╋┫╭━╋┫╭╮┃┃')
+        await ctx.channel.send('╭╯╰╯┃┃━╋━━┃┃━┫┃┃╰┫┃━┫╰╯┃┃╰━╯┃┃┃╱┃┃┃┃╰━┫┃╭╮┃╰╮')
+        await ctx.channel.send('╰━━━┻━━┻━━┻━━┻╯╰━┻━━┻━━╯╰━━━╯╰╯╱╰╯╰┻━━┻┻╯╰┻━╯')
 
 
 
 @client.event
 async def on_ready():
     print('Main Is Running')
+    print(extensionsrun)
+    print('    ___                        _             _     ___   __   __  _        _         _ ')
+    print('   /   \ ___  ___   ___  _ __ | |_  ___   __| |   /___\ / _| / _|(_)  ___ (_)  __ _ | |')
+    print('  / /\ // _ \/ __| / _ \| `__|| __|/ _ \ / _` |  //  //| |_ | |_ | | / __|| | / _` || |')
+    print(' / /_//|  __/\__ \|  __/| |   | |_|  __/| (_| | / \_// |  _||  _|| || (__ | || (_| || |')
+    print('/____/  \___||___/ \___||_|    \__|\___| \__,_| \___/  |_|  |_|  |_| \___||_| \__,_||_|')
+    print('')
+    print('                                                 $$$$$')
+    print('                                     $$$$       $$$   $$$$$')
+    print('                                    $$$$$$     $$$   $$$$$$$')
+    print('                                   $$$ $$$$   $$$  $$$$    $')
+    print('                                   $    $$$$ $$$$ $$$')
+    print('                                        $$$$$$$$$$   $$$$$')
+    print('                                       $$$$$$$$$$$$_$$$$ $$$$$')
+    print('                                      $$$$$$$$$$$$$$$$$     $$$')
+    print('                                     $$$  $$$$$$$$$$$$        $')
+    print('                                    $$   $$$$$$$$$$$$$$$$$')
+    print('                                   $$   $$$$ $$$ $$$  $$$$$')
+    print('                                   $   $$$$   $$  $$$$   $$$')
+    print('                                       $$$     $$   $$$    $$')
+    print('                                       $$      $$$   $$$    $')
+    print('                                       $$       $$    $$$')
+    print('                                       $        $$$    $$')
+    print('                                                $$$    $')
+    print('                                                $$$')
+    print('                                                $$$')
+    print('                                                $$$')
+    print('                                                $$$')
+    print('                                               $$$')
+    print('                                              $$$$')
+    print('                                            $$$$$')
+    print('                                           $$$$$$')
+    print('')
+                                                                                       
+
+    
 client.run(TOKEN)
--- README.md
+++ README.md
@@ -1,39 +1,70 @@
 # DesertedBot
-This bot Has 5 public commands:
+This bot Has Many public commands:
 
-- dm                                                                                                                                                                
-Type Command Like:                                                                                                                                                   
-  !dm @(mention the user you want to dm) (your message here)                                                                                                         
-This will send a dm as a discord bot                                                                                                                                 
-                                                                                                                                                   
-- meme                                                                                                                                                    
-Type Command Like:                                                                                                                                                   
-  !meme                                                                                                                                                   
-This will terribly generate a random meme                                                                                                                           
-                                                                                                                                                   
-- ServerInfo                                                                                                                                                    
-Type Command Like:                                                                                                                                                   
-  !Serverinfo                                                                                                                                                   
-This will provide you with interesting stats on the server                                                                                                           
-                                                                                                                                                   
-- UserInfo                                                                                                                                                    
-Typer Command Like:                                                                                                                                                
-  !Info @(user you want to info on if you dont mention anyone it will give info on you)                                                                             
-This will provide you with stats on given user                                                                                                                                                                                                                                                                       
-- Toggle                                                                                                                                                    
-Type Command Like:                                                                                                                                                   
-  !load (command)                                                                                                                                                   
-  !unload (command)                                                                                                                                                 
-  !loadall                                                                                                                                                   
-  !unloadall                                                                                                                                                   
-Load will activate given command, Unload will deactivate given command, LoadAll will load all commands, UnloadAll will unload all commands                           
-                                                                                                                                                   
-- RunningInfo                                                                                                                                                    
-Type Command Like:                                                                                                                                                   
-  !runninginfo                                                                                                                                                       
-NOTE: you cannot unload this command. This command will show you witch commands are running                                                                         
-                                                                                                                                                                     
-- ChangeCP                                                                                                                                                           
-Type Command Like:                                                                                                                                                   
-  !ChangeCP (what you want to change command prefix to)                                                                                                             
-A command prefix is the ! at the beginning of every command. This command will change the command prefix                                                             
+#### **Dm**
+Type Command Like:
+>>*!dm* @(mention the user you want to dm) (your message here)\
+
+This will send a dm as a discord bot\
+
+#### **BanTxT**
+Type Command Like:
+>>*!Tban* @(mention the user) (reason)\
+>>*!UTban* @(mention user)\
+
+Tban will stop the mentioned user from texting in any channel\
+UTban will let select user to text again\
+
+#### **Spam**
+Type Command Like:
+>>*!Spamdm* @(mention the user to spam) (message to spam)\
+>>*!Spam* (message to spam)\
+
+Spamdm will spam only 1 person with the message you provided\
+Spam will spam that channel with provided message\
+
+#### **AutoReact**
+Type Command Like:
+>>*!addreact* (the emoji you want to auto react)\
+
+This will automatically react to messages in that channel with provided emoji\
+
+#### **meme**
+Type Command Like:
+>>*!meme*\
+
+This will terribly generate a random meme\
+
+#### **ServerInfo**
+Type Command Like:
+>>*!Serverinfo*\
+
+This will provide you with interesting stats on the server\
+
+#### **UserInfo**
+Typer Command Like:\
+>>*!Info* @(user you want to info on if you dont mention anyone it will give info on you)\
+
+This will provide you with stats on given user\
+
+#### **Toggle**
+Type Command Like:
+>>*!load* (command)\
+>>*!unload* (command)\
+>>*!loadall*\
+>>*!unloadall*\
+
+Load will activate given command, Unload will deactivate given command, LoadAll will load all commands, UnloadAll will unload all commands\
+
+#### **RunningInfo**
+Type Command Like:
+>>*!runninginfo*\
+
+Runninginfo will provide the extensions running\
+**NOTE**: you cannot unload this command. This command will show you witch commands are running\
+
+#### **ChangeCP**
+Type Command Like:
+>>*!ChangeCP* (what you want to change command prefix to)\
+
+A command prefix is the ! at the beginning of every command. This command will change the command prefix\
